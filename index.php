7bde151e6402057e69553662136a9483d6596174
Yago Inácio <yagofaran@gmail.com>
2018-10-11 23:57:28 -0300

    added button to toggle led

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<form method=get >
+    <input type=submit value='Meu Botão' />
+</form>
+
 <?php
 
 require("phpMQTT/phpMQTT.php");
@@ -6,9 +10,13 @@ $mqtt = new phpMQTT("localhost", 1234, "MQTT Publisher Client");
 
 if ($mqtt->connect()) {
 	//$mqtt->publish("yasir/ali/test/topic","Hello World! at ".date("r"),0);
-	$mqtt->publish("casa/quarto/yago","lamp",0);
+	if (isset($_GET)){
+		$mqtt->publish("casa/quarto/yago","lamp",0);
+		$mqtt->close();
+	}
 	echo "oie";
-	$mqtt->close();
+	
 }
+
 
 ?>
