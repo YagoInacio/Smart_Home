10db318c5dacc05a9920916f6899f6e32c76fc79
Yago Inácio <yagofaran@gmail.com>
2018-10-12 00:22:58 -0300

    add config.php to implement autoload and corretion of a bug that made the led toggle whenever the page was loaded or refreshed

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
-<form method=get >
-    <input type=submit value='Meu Botão' />
+<form action="?a=ok" method="POST" >
+    <input type=submit value='Lampada 1' />
 </form>
 
 <?php
 
-require("phpMQTT/phpMQTT.php");
+require_once("config.php");
  
 $mqtt = new phpMQTT("localhost", 1234, "MQTT Publisher Client");
 
 if ($mqtt->connect()) {
 	//$mqtt->publish("yasir/ali/test/topic","Hello World! at ".date("r"),0);
-	if (isset($_GET)){
-		$mqtt->publish("casa/quarto/yago","lamp",0);
-		$mqtt->close();
-	}
+
 	echo "oie";
 	
+}
+
+if (isset($_GET['a'])){
+	$mqtt->publish("casa/quarto/yago","lamp",0);
+	$mqtt->close();
 }
 
 
