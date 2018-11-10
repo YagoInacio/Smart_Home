04efbbd5495fca03bc6a2085920046187644c584
Yago Inácio <yagofaran@gmail.com>
2018-11-10 15:59:13 -0300

    added echo for when the mqtt server can not connect

--- index.php
+++ index.php
@@ -14,6 +14,9 @@ $mqtt = new phpMQTT("localhost", 1234, "MQTT Publisher Client");
 if ($mqtt->connect()) {
 	echo "MQTT Connected!";
 }
+else{
+	echo "MQTT Could Not Connect"
+}
 
 if (isset($_GET['a']) && $_GET['a'] == 'ok'){
 	$mqtt->publish("casa/quarto/yago","lamp",0);
