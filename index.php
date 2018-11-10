49fe5812caac712e1eccc259240e5f8757bcf14b
Yago Inácio <yagofaran@gmail.com>
2018-11-10 15:18:48 -0300

    test with 2 forms for 2 diferent lamps

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <form action="?a=ok" method="POST" >
-    <input type=submit value='Lampada 1' />
+    <input type=submit value='Lamp 1' />
+</form>
+<form action="?a=notok" method="POST" >
+    <input type=submit value='Lamp 2' />
 </form>
 
 <?php
@@ -9,13 +12,10 @@ require_once("config.php");
 $mqtt = new phpMQTT("localhost", 1234, "MQTT Publisher Client");
 
 if ($mqtt->connect()) {
-	//$mqtt->publish("yasir/ali/test/topic","Hello World! at ".date("r"),0);
-
-	echo "oie";
-	
+	echo "MQTT Connected!";
 }
 
-if (isset($_GET['a'])){
+if (isset($_GET['a']) && $_GET['a'] == 'ok'){
 	$mqtt->publish("casa/quarto/yago","lamp",0);
 	$mqtt->close();
 }
